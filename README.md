5a23d88db1319d6817a81cd6e89d565a9d127636
mug896 <mug896@gmail.com>
2020-06-23 13:42:22 +0900

    update

--- README.md
+++ README.md
@@ -1,2 +1,102 @@
-# awk-json-search
-awk script for json search
+# awk script for jsone search
+
+## 1. Pretty printing
+
+```bash
+$ cat sample.json
+{"squadName":"Super hero squad","homeTown":"Metro City","formed":2016,"secretBase":
+"Super tower","active":true,"members":[{"name":"Molecule Man","age":29,"secretIdentity":
+"Dan Jukes","powers":["Radiation resistance","Turning tiny","Radiation blast"]},{"name":
+"Madame Uppercut","age":39,"secretIdentity":"Jane Wilson","powers":["Million tonne punch",
+"Damage resistance","Superhuman reflexes"]},{"name":"Eternal Flame","age":1000000,
+"secretIdentity":"Unknown","powers":["Immortality","Heat Immunity","Teleportation",
+"Interdimensional travel"]}]}
+
+$ ./json.sh sample.json
+
+{
+  "squadName": "Super hero squad",
+  "homeTown": "Metro City",
+  "formed": 2016,
+  "secretBase": "Super tower",
+  "active": true,
+  "members": [
+    {
+      "name": "Molecule Man",
+      "age": 29,
+      "secretIdentity": "Dan Jukes",
+      "powers": [
+        "Radiation resistance",
+        "Turning tiny",
+        "Radiation blast"
+      ]
+    },
+    {
+      "name": "Madame Uppercut",
+      "age": 39,
+      "secretIdentity": "Jane Wilson",
+      "powers": [
+        "Million tonne punch",
+        "Damage resistance",
+        "Superhuman reflexes"
+      ]
+    },
+    {
+      "name": "Eternal Flame",
+      "age": 1000000,
+      "secretIdentity": "Unknown",
+      "powers": [
+        "Immortality",
+        "Heat Immunity",
+        "Teleportation",
+        "Interdimensional travel"
+      ]
+    }
+  ]
+}
+```
+
+
+## 2. Searching
+
+```bash
+$ ./json.sh -k sample.json       # 전체 key 값을 출력
+/squadName
+/homeTown
+/formed
+/secretBase
+/active
+/members
+/members/name
+/members/age
+/members/secretIdentity
+/members/powers
+/members/name
+/members/age
+/members/secretIdentity
+/members/powers
+/members/name
+/members/age
+/members/secretIdentity
+/members/powers
+
+# key 값이 /members/name 인 value 를 모두 출력
+$ ./json.sh -s /members/name sample.json
+"Molecule Man"
+"Madame Uppercut"
+"Eternal Flame"
+
+# /members/name 의 value 가 "Molecule Man" 인 전체 블록을 출력
+$ ./json.sh -s /members/name="Molecule Man" sample.json 
+
+{
+  "name": "Molecule Man",
+  "age": 29,
+  "secretIdentity": "Dan Jukes",
+  "powers": [
+    "Radiation resistance",
+    "Turning tiny",
+    "Radiation blast"
+  ]
+}
+```
---
+++ json.sh
@@ -0,0 +1,134 @@
+#!/usr/bin/env -S gawk -E
+
+BEGIN {
+    RS="(.)"
+    INDENT = 2;
+    keyword["true"]; keyword["false"]; keyword["null"]
+    token[0] = opt1 = opt2 = ""
+    idx = sdx = sblock = 0
+}
+
+BEGINFILE {
+    if ( FILENAME ~ /^-k$|^-s$/ ) { opt1 = FILENAME; nextfile }
+    if ( opt1 == "-s" && opt2 == "" ) { opt2 = FILENAME; nextfile }
+}
+
+{ tokenize(RT) }
+
+END { 
+    if (opt1) search(0, ""); else pretty_print(0)
+}
+
+
+function search (start, path,       ckey, key, val) {
+    for (sdx = start; sdx < idx; sdx++) { 
+        switch (token[sdx]) {
+            case "{" :
+            case "[" : 
+                sblock = sdx
+                search( sdx + 1, path "/" ckey)
+                break
+            case "}" :
+            case "]" : return
+            case ":" : 
+                ckey = gensub( /^"(.*)"$/, "\\1", "g", token[sdx - 1])
+                key  = gensub( /\/{2,}/, "/", "g", path "/" ckey)
+                if ( opt1 == "-k" ) print key 
+                else if ( opt1 == "-s" ) {
+                    if ( opt2 == key ) {
+                        switch( token[sdx + 1] ) {
+                            case "[" :
+                            case "{" : pretty_print(sdx + 1); break
+                            default  : print token[sdx + 1]
+                        }
+                    } else {
+                        val = gensub( /^"(.*)"$/, "\\1", "g", token[sdx + 1])
+                        if ( opt2 == key "=" val ) pretty_print(sblock)
+                    }
+                }
+        }
+    }
+}
+
+function tokenize (char) {
+    switch (char) {
+        case /{|}|\[|]|:|,/  : token[idx++] = char; break
+        case "\""            : t_string(); break
+        case /[0-9-]/        : t_number(char); break
+        case /[a-z]/         : t_keyword(char); break
+    }
+}
+
+function t_keyword (str) {
+    while (getline) {
+        if ( RT ~ /[a-z]/ ) 
+            str = str RT
+        else {
+            if ( str in keyword ) token[idx++] = str
+            break
+        }
+    }
+    tokenize(RT)
+}
+
+function t_number (str) {
+    while (getline) {
+        if ( RT ~ /[0-9.eE+-]/ ) 
+            str = str RT
+        else {
+            token[idx++] = str
+            break
+        }
+    }
+    tokenize(RT)
+}
+
+function t_string (    str, prev) {
+    while (getline) {
+        if ( RT != "\"" ) 
+            str = str RT
+        else {
+            if ( prev == "\\" ) {
+                str = str RT
+            } else {
+                token[idx++] = "\"" str "\""
+                break
+            }
+        }
+        prev = RT
+    }
+}
+
+function space (depth,   i, sp) { 
+    depth = depth * INDENT
+    for (i=0; i < depth; i++) sp = sp " "
+    return sp
+}
+
+function pretty_print (start,    i, depth, prev, cur) {
+    for (i = start; i < idx; i++) {
+        prev = cur; cur = token[i]
+        switch (cur) {
+            case "{" : 
+                printf (prev == ":" ? "" : "\n" space(depth)) "{"
+                depth++; break
+            case "}" : 
+                depth--
+                printf (prev == "{" ? " " : "\n" space(depth)) "}"
+                break
+            case "[" : 
+                printf (prev == ":" ? "" : "\n" space(depth)) "["
+                depth++; break
+            case "]" : 
+                depth--
+                printf (prev == "[" ? " " : "\n" space(depth)) "]"
+                break
+            case "," : printf(","); break
+            case ":" : printf(": "); break
+            default : 
+                printf (prev == ":" ? "" : "\n" space(depth)) cur
+        }
+        if (depth == 0) break
+    }
+    print
+}
